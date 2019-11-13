1b0c3ad4de840b7a7a26cf6761d986bcdb41af8b
ColinEspinas <colin.espinas@yahoo.fr>
2019-11-14 00:19:39 +0100

    minor changes for github pages

--- config/config.js
+++ config/config.js
@@ -5,7 +5,7 @@ let config;
 function loadConfig(callback) {   
 	let xhr = new XMLHttpRequest();
 	xhr.overrideMimeType("application/json");
-	xhr.open('GET', '/config/config.json', true);
+	xhr.open('GET', './config/config.json', true);
 	xhr.onreadystatechange = function () {
 		if (xhr.readyState == 4 && xhr.status == "200") {
 			callback(xhr.responseText);
