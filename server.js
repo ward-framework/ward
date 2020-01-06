9abdb3e822192313d5ac789c622b730270b35111
ColinEspinas <colin.espinas@yahoo.fr>
2020-01-06 20:44:49 +0100

    changed server ip to 0.0.0.0

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const Server = require('ward-server');
 // Create a Server instance
 const server = new Server({
 	path : __dirname,
-	port: process.env.PORT || 8000
+	port: process.env.PORT || 8000,
+	ip: "0.0.0.0"
 });
 
 // Serve files
